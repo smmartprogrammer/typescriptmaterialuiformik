834cd0ebe091d6a46238610833d88e094476541c
Raghunathji <partabrajp@gmail.com>
2021-05-15 18:52:24 +0500

    uploading project to git

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+interface FormValues {
+  name: string;
+}
+
+const initialValues: FormValues = {
+  name: '',
+};
+
+const registerSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Name must have atleast 2 characters')
+    .required('Name is require'),
+});
 
 function App() {
+  const forSubmit = (values: FormValues): void => {
+    alert(JSON.stringify(values));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Register</h1>
+      <Formik initialValues={initialValues} onSubmit={forSubmit}>
+        {(props) => {
+          return (
+            <Form>
+              <div>
+                <label>Name:</label>
+                <Field name="name" as="input" />
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
